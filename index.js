1f6733ab9bef8d0d4e7407fd952d99949d174698
Gobinath26 <gobi3336@gmail.com>
2022-05-21 16:31:06 +0530

    .

--- index.js
+++ index.js
@@ -22,53 +22,6 @@ async function createConnection() {
 }
 const client = await createConnection();
 
-// const flipkart = [
-//   {
-//     "id": "100",
-//     "image":
-//       "https://www.redwolf.in/image/catalog/mens-t-shirts/full-sleeves/batman-emblem-full-sleeve-t-shirt-india.jpg",
-//     "tittle": "T-Shirt",
-//     "rating": 3.9,
-//     "price": "₹1,299",
-//     "finalpricewithoffer": "₹199 (84%off)",
-//   },
-//   {
-//     "id": "101",
-//     "image":
-//       "https://www.trustedreviews.com/wp-content/uploads/sites/54/2019/10/iphone11promax-1-1.jpeg",
-//     "tittle": "APPLE iPhone 11 Pro Max (Space Grey, 64 GB)",
-//     "rating": 4.7,
-//     "price": "₹1,17,100",
-//     "finalpricewithoffer": "₹1,12,000",
-//   },
-//   {
-//     "id": "102",
-//     "image":
-//       "https://image1.pricedekho.com/p/35/3/13/3419013/27948861-new-fancy-watch-men-and-women-pack-2-analog-watch-for-men-women-by-7star-picture-home.jpg",
-//     "tittle": "Watch For Men & Women",
-//     "rating": 3.8,
-//     "price": "₹6,999",
-//     "final price with offer": "₹3,099(55% off)",
-//   },
-//   {
-//     "id": "103",
-//     "image":
-//       "https://www.photoreview.com.au/wp-content/uploads/images/media/images2018/eos-1500d-back2/2783263-1-eng-GB/eos-1500d-back.jpg",
-//     "tittle": "Canon EOS 1500D DSLR Camera Body+ 18-55 mm IS II Lens  (Black)",
-//     "rating": 4.5,
-//     "price": "₹39,999",
-//     "finalpricewithoffer": "₹36,499",
-//   },
-//   {
-//     "id": "104",
-//     "image":
-//       "https://www.seekdevice.com/news/wp-content/uploads/2021/06/legion-5-pro-1.jpg",
-//     "tittle": "Lenovo Legion 5 Ryzen 5 Hexa Core 4600H Laptop",
-//     "rating": 4.5,
-//     "price": "₹93,690",
-//     "finalpricewithoffer": "₹63,940 (31% off)",
-//   }
-// ];
 
 app.use(express.json());
 app.use(cors());
